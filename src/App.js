f44ff7d6d1ad3bd5791a6d18c1167748939f57f7
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2019-01-16 16:05:03 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Save Me Some Space</h2>
+          <p>A web app to store data to different cloud providers</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
